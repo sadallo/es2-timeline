609466491cfcf45961161896dcb63ea2892176fb
sadallo <sadallo@hotmail.com>
2018-09-24 21:25:27 -0300

    segundo commit do modulo de timeline

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 import os
 from flask import Flask
 import psycopg2
-from consultas import Consulta
-import data
 
 app = Flask(__name__)
 
-@app.route('/getuser/<identificador>')
-def getuser(identificador):
-	user = Consulta().getuser(identificador)
-	return user
-
 @app.route('/cadastrar/<login>&<name>&<bio>')
 def cadastrar(login, name, bio):
-	Consulta().cadastrar(login, name, bio)
+	#Consulta().cadastrar(login, name, bio)
 	return ""
 
 
@@ -24,4 +17,4 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
